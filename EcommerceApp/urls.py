dd8827d791cea97e8a081fa3336c994b761d52d7
emmanuelmingo​ <mingoemmanuel06@gmail.com>
2026-08-11 16:37:14 +0100

    Fix a bug in the urls.py

--- EcommerceApp/urls.py
+++ EcommerceApp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name="signup"),
-    path('verify-email/<str:token>', views.verify_emial, name="Verify-Email")
+    path('verify-email/<str:token>', views.verify_email, name="Verify-Email")
 ]
